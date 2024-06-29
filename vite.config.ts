30e656be601bfc68dcb537f1c7baf402e92635bd
Divyam <divyam2203@gmail.com>
2024-06-30 00:22:24 +0530

    testing 02

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Github-Explorer",
+  base: "https://divyam2203.github.io/GitHub-Explorer/",
   plugins: [react()],
 })
