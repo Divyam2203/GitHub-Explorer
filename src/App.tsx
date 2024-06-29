1e1edf539c9b387ae3c58b4216d2734af24248cb
Divyam <divyam2203@gmail.com>
2024-06-30 00:14:17 +0530

    testing 02

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ import ViewArea from "./components/ViewArea";
 
 //  `https://api.github.com/search/repositories?q=${data.text}`,
 export default function App() {
-
   const [inputString, setInputString] = useState("");
   const [receivedData, setReceivedData] = useState<any | null>({
-    total_count:0,
+    total_count: 0,
     incomplete_results: false,
-    items:[],
+    items: [],
   });
 
-
+  let incompleteResults = true;
 
   const getData = async (query: string) => {
     try {
@@ -21,7 +20,6 @@ export default function App() {
         `http://api.github.com/search/repositories?q=${query}&per_page=10`,
       );
       setReceivedData(response.data);
-      //console.log(receivedData)
     } catch (err) {
       console.log(err);
     }
@@ -32,15 +30,14 @@ export default function App() {
     const fd = new FormData(event.target);
     const data = Object.fromEntries(fd.entries());
     setInputString(data.text as string);
-    console.log(inputString);
-    
+    //console.log(inputString);
   }
 
   useEffect(() => {
     let timerOut = setTimeout(() => {
       //setHideDetails(true);
       if (inputString !== "") {
-        console.log("fetching")
+        console.log("fetching");
         getData(inputString as string);
         //viewRef.current?.scrollIntoView();
       }
@@ -50,11 +47,16 @@ export default function App() {
       clearTimeout(timerOut);
     };
   }, [inputString]);
-
+  incompleteResults = receivedData.incomplete_results;
+  //console.log(incompleteResults);
   return (
-    <div className="m-0 p-0 h-screen overflow-auto">     
+    <div className="m-0 h-screen overflow-auto p-0">
       <Hero handleSearch={handleSearch} />
-      <ViewArea data={receivedData} />
+      {incompleteResults === true ? (
+        <p>null</p>
+      ) : (
+        <ViewArea data={receivedData} />
+      )}
     </div>
   );
 }
--- src/components/ViewArea.tsx
+++ src/components/ViewArea.tsx
@@ -3,11 +3,11 @@ import Cards from "./Cards";
 
 export default function ViewArea({data}:any) {
   //let items = data.items;
-  console.log(data)
+  //console.log(data)
   return (
     <>
     <div className="mx-8 flex flex-col justify-center mb-20">
-      {data.items.map((item:any)=>(<Cards item={item} />))}
+      {data.items.map((item:any)=>(<Cards item={item} key={item.id} />))}
       </div>
     </>
   );
